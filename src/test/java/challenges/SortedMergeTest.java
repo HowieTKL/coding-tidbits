1ec210b75aa4059298bdccad72ac208e72c6f7f0
HowieTKL <HowieTKL@users.noreply.github.com>
2024-11-03 18:29:52 -0500

    Code analysis fixes
    Code analysis fixes
    
    Code analysis fixes

--- src/test/java/challenges/SortedMergeTest.java
+++ src/test/java/challenges/SortedMergeTest.java
@@ -14,41 +14,41 @@ public class SortedMergeTest {
    */
   @Test
   public void testSort1() {
-    Comparable[] a = new Integer[]{1, 3};
-    Comparable[] b = new Integer[]{2, 4};
-    SortedMerge instance = new SortedMerge();
-    Comparable[] expResult = new Integer[]{1, 2, 3, 4};
-    Comparable[] result = instance.sort(a, b);
+    Integer[] a = new Integer[]{1, 3};
+    Integer[] b = new Integer[]{2, 4};
+    SortedMerge<Integer> instance = new SortedMerge<>();
+    Integer[] expResult = new Integer[]{1, 2, 3, 4};
+    Integer[] result = instance.sort(a, b);
     assertArrayEquals(expResult, result);
   }
   
   @Test
   public void testSort2() {
-    Comparable[] a = new Integer[]{1, 9};
-    Comparable[] b = new Integer[]{5, 6, 10};
-    SortedMerge instance = new SortedMerge();
-    Comparable[] expResult = new Integer[]{1, 5, 6, 9, 10};
-    Comparable[] result = instance.sort(a, b);
+    Integer[] a = new Integer[]{1, 9};
+    Integer[] b = new Integer[]{5, 6, 10};
+    SortedMerge<Integer> instance = new SortedMerge<>();
+    Integer[] expResult = new Integer[]{1, 5, 6, 9, 10};
+    Integer[] result = instance.sort(a, b);
     assertArrayEquals(expResult, result);
   }
 
   @Test
   public void testSort3() {
-    Comparable[] a = new Integer[]{1, 9, 22};
-    Comparable[] b = new Integer[]{5, 6};
-    SortedMerge instance = new SortedMerge();
-    Comparable[] expResult = new Integer[]{1, 5, 6, 9, 22};
-    Comparable[] result = instance.sort(a, b);
+    Integer[] a = new Integer[]{1, 9, 22};
+    Integer[] b = new Integer[]{5, 6};
+    SortedMerge<Integer> instance = new SortedMerge<>();
+    Integer[] expResult = new Integer[]{1, 5, 6, 9, 22};
+    Integer[] result = instance.sort(a, b);
     assertArrayEquals(expResult, result);
   }
  
   @Test
   public void testSortEmpty() {
-    Comparable[] a = new Integer[]{};
-    Comparable[] b = new Integer[]{};
-    SortedMerge instance = new SortedMerge();
-    Comparable[] expResult = new Integer[]{};
-    Comparable[] result = instance.sort(a, b);
+    Integer[] a = new Integer[]{};
+    Integer[] b = new Integer[]{};
+    SortedMerge<Integer> instance = new SortedMerge<>();
+    Integer[] expResult = new Integer[]{};
+    Integer[] result = instance.sort(a, b);
     assertArrayEquals(expResult, result);
   }
   
