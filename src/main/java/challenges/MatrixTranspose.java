416d1915e74644131ed7454663787aaff63ea4e5
HowieTKL <HowieTKL@users.noreply.github.com>
2024-11-03 18:18:38 -0500

    Add MatrixTranspose & MatrixTransposeTest

---
+++ src/main/java/challenges/MatrixTranspose.java
@@ -0,0 +1,16 @@
+package challenges;
+
+public class MatrixTranspose {
+
+    public int[][] transpose(int[][] matrix) {
+        int[][] transposed = new int[matrix[0].length][matrix.length];
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+}
